23df7a1f39b1cc0f0169abb52cb965401347d6fe
Xin Yao <yaoxyx@outlook.com>
2022-03-15 17:14:57 +0800

    Update gravity_model.py

--- baselines/gravity_model.py
+++ baselines/gravity_model.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
 from sklearn.linear_model import LinearRegression
 
 from func import *
@@ -16,7 +15,6 @@ def fit(flows, features):
         Y.append(np.log(intensity))
         X.append([np.log(feat_o[2]), np.log(feat_d[2]),
                   np.log(haversine_distance(feat_o[0], feat_o[1], feat_d[0], feat_d[1]))])
-        print(haversine_distance(feat_o[0], feat_o[1], feat_d[0], feat_d[1]))
 
     reg = LinearRegression().fit(X, Y)
     beta = reg.coef_
@@ -31,7 +29,6 @@ def predict(flows, features, beta, K):
     for f in flows:
         p.append(K * (features[f[0]][2] ** beta[0]) * (features[f[1]][2] ** beta[1]) *
                  (haversine_distance(features[f[0]][0], features[f[0]][1], features[f[1]][0], features[f[1]][1]) ** beta[2]))
-        print(haversine_distance(features[f[0]][0], features[f[0]][1], features[f[1]][0], features[f[1]][1]))
         r.append(f[2])
 
     return p, r
@@ -39,13 +36,12 @@ def predict(flows, features, beta, K):
 
 if __name__ == '__main__':
     path = '../data/simple/'
-    tr_f = read_flows(path + 'train.txt')
-    te_f = read_flows(path + 'test.txt')
-    # Data format: [region_name, x/lng, y/lat, attractiveness]
-    features = read_features(path + 'features.txt')
+    train_f = read_flows(path + 'train.txt')  # Flow list: [origin, destination, intensity]
+    test_f = read_flows(path + 'test.txt')
+    features = read_features(path + 'features.txt')  # Feature dict: [x/lng, y/lat, feature]
 
-    beta, K = fit(tr_f, features)
-    pred, real = predict(te_f, features, beta, K)
+    beta, K = fit(train_f, features)
+    pred, real = predict(test_f, features, beta, K)
 
     print('beta =', beta, ', K =', K)
     # np.savetxt('../data/pred_GM_P.txt', pred, delimiter=',')

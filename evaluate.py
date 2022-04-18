07fc0f0fa1e1450c602fc7410b7eec6202a5ceac
Xin Yao <yaoxyx@outlook.com>
2022-04-18 15:38:50 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -9,9 +9,11 @@ import torch
 
 from utils import load_graph_data, load_flow_data, read_setting
 from models import RGCN
+import os
+import shutil
 
 
-def main(args):
+def evaluate(args, file_mark):
     setting = read_setting(args.setting)
 
     entity2id, relation2id, embedding_graph = load_graph_data(args.dataset)
@@ -20,28 +22,46 @@ def main(args):
     model = RGCN(len(entity2id), len(relation2id), num_bases=int(setting['n_bases']),
                  embedding_size=int(setting['embedding_size']), dropout=float(setting['dropout']))
 
-    checkpoint = torch.load('./saved_model/'+args.model)
+    checkpoint = torch.load('./model/'+args.model)
     model.load_state_dict(checkpoint['state_dict'])
 
     model.eval()
 
     entity_embedding = model(embedding_graph.entity, embedding_graph.edge_index, embedding_graph.edge_type,
                              embedding_graph.edge_norm)
-    test_mse = model.score_loss(entity_embedding, test_samples, test_labels)
 
-    print('\nMSE of test data: {}'.format(test_mse))
+    rmse = model.score_rmse(entity_embedding, test_samples, test_labels)
+    mape = model.score_mape(entity_embedding, test_samples, test_labels)
+    cpc = model.score_cpc(entity_embedding, test_samples, test_labels)
+    r2 = model.score_r2(entity_embedding, test_samples, test_labels)
+
+    model.export_results(entity_embedding, test_samples, test_labels).to_csv('result/'+file_mark+'_eval.csv',
+                                                                             header=True, encoding='gbk')
+
+    return [rmse, mape, cpc, r2]
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='RGCN')
-    parser.add_argument("--dataset", type=str, default="./data/simple")
-    parser.add_argument("--setting", type=str, default="./setting.txt")
-    parser.add_argument("--model", type=str, default='simple_20211203-17-48-13.pth')
+    model_path = 'model/'
+    with open('metrics.csv', 'w') as f:
+        f.write('model,rmse,mape,cpc,r2\r\n')
+        for file in os.listdir(model_path):
+            if file[-3:] == 'pth':
+                print(file)
+                file_mark = file[0:-4]
+                # Attention: Replace the graph file that is used for training
+                shutil.copy2('data/graph_knn/graph_30.txt', 'data/mobility/graph.txt')
 
-    args = parser.parse_args()
-    print('Data path:')
-    print('\t' + args.dataset)
-    print('Setting file:')
-    print('\t' + args.setting)
+                parser = argparse.ArgumentParser(description='RGCN')
+                parser.add_argument("--dataset", type=str, default="./data/mobility")
+                parser.add_argument("--setting", type=str, default="./setting.txt")
+                parser.add_argument("--model", type=str, default=file)
 
-    main(args)
+                args = parser.parse_args()
+                print('Data path:')
+                print('\t' + args.dataset)
+                print('Setting file:')
+                print('\t' + args.setting)
+
+                r = evaluate(args, file_mark)
+                f.write(','.join([file_mark] + list(map(str, r)))+'\r\n')

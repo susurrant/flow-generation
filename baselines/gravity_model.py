09727ca5e0d8358c8ec8269bde721f8cfd421d06
Xin Yao <yaoxyx@outlook.com>
2022-04-18 15:40:16 +0800

    Update gravity_model.py

--- baselines/gravity_model.py
+++ baselines/gravity_model.py
@@ -12,8 +12,12 @@ def fit(flows, features):
         intensity = k[2]
         feat_o = features[k[0]]
         feat_d = features[k[1]]
+        if feat_o[2] == 0:
+            feat_o[2] = 1
+        if feat_d[3] == 0:
+            feat_d[3] = 1
         Y.append(np.log(intensity))
-        X.append([np.log(feat_o[2]), np.log(feat_d[2]),
+        X.append([np.log(feat_o[2]), np.log(feat_d[3]),
                   np.log(haversine_distance(feat_o[0], feat_o[1], feat_d[0], feat_d[1]))])
 
     reg = LinearRegression().fit(X, Y)
@@ -27,7 +31,14 @@ def predict(flows, features, beta, K):
     p = []
     r = []
     for f in flows:
-        p.append(K * (features[f[0]][2] ** beta[0]) * (features[f[1]][2] ** beta[1]) *
+        feat_o = features[f[0]][2]
+        if feat_o == 0:
+            feat_o = 1
+        feat_d = features[f[1]][3]
+        if feat_d == 0:
+            feat_d = 1
+
+        p.append(K * (feat_o ** beta[0]) * (feat_d ** beta[1]) *
                  (haversine_distance(features[f[0]][0], features[f[0]][1], features[f[1]][0], features[f[1]][1]) ** beta[2]))
         r.append(f[2])
 
@@ -35,14 +46,15 @@ def predict(flows, features, beta, K):
 
 
 if __name__ == '__main__':
-    path = '../data/simple/'
-    train_f = read_flows(path + 'train.txt')  # Flow list: [origin, destination, intensity]
-    test_f = read_flows(path + 'test.txt')
-    features = read_features(path + 'features.txt')  # Feature dict: [x/lng, y/lat, feature]
+    path = '../data/'
+    train_f = read_flows([path + 'train.txt', path + 'valid.txt'])  # Flow list: [origin, destination, intensity]
+    test_f = read_flows([path + 'test.txt'])
+    features = read_features(path + 'features_pop.txt')  # Feature dict: [x/lng, y/lat, push/outgoing, pull/incoming]
 
     beta, K = fit(train_f, features)
     pred, real = predict(test_f, features, beta, K)
 
     print('beta =', beta, ', K =', K)
-    # np.savetxt('../data/pred_GM_P.txt', pred, delimiter=',')
+    result = np.concatenate((np.array(real).reshape((-1, 1)), np.array(pred).reshape((-1, 1))), axis=1)
+    np.savetxt('pred_gm.txt', result, delimiter=',')
     evaluate(pred, real)

8bb4c1ef98c7f1349b2476499d349dffbb939adc
Xin Yao <yaoxyx@outlook.com>
2022-06-15 17:01:46 +0800

    Update models.py

--- models.py
+++ models.py
@@ -30,10 +30,8 @@ class RGCN(torch.nn.Module):
         self.relation_embedding = nn.Parameter(torch.Tensor(1, embedding_size, embedding_size))
         self.bias = nn.Parameter(torch.Tensor(1))
 
-        nn.init.xavier_uniform_(self.relation_embedding)  # gain=nn.init.calculate_gain('relu')
-        nn.init.constant_(self.bias, bias)
-
         nn.init.xavier_uniform_(self.relation_embedding, gain=nn.init.calculate_gain('relu'))
+        nn.init.constant_(self.bias, bias)
 
         self.conv1 = RGCNConv(
             embedding_size, embedding_size, num_relations * 2, num_bases=num_bases)

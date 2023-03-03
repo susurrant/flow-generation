e27be1d6c949f94e47a94ed16ff91730eb7fd847
Xin Yao <yaoxyx@outlook.com>
2023-03-03 16:17:37 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -9,20 +9,20 @@ import torch
 
 from utils import load_graph_data, load_flow_data, read_setting
 from models import RGCN
+import numpy as np
 import os
-import shutil
 
 
-def evaluate(args, file_mark):
+def main(args, file_mark):
     setting = read_setting(args.setting)
 
-    entity2id, relation2id, embedding_graph = load_graph_data(args.dataset, verbose=0)
+    entity2id, relation2id, embedding_graph = load_graph_data(args.dataset)
     test_samples, test_labels = load_flow_data(args.dataset, entity2id, mode='test')
 
     model = RGCN(len(entity2id), len(relation2id), num_bases=int(setting['n_bases']),
                  embedding_size=int(setting['embedding_size']), dropout=float(setting['dropout']))
 
-    checkpoint = torch.load('saved_model/'+args.model)
+    checkpoint = torch.load('output/'+args.model)
     model.load_state_dict(checkpoint['state_dict'])
 
     model.eval()
@@ -30,40 +30,61 @@ def evaluate(args, file_mark):
     entity_embedding = model(embedding_graph.entity, embedding_graph.edge_index, embedding_graph.edge_type,
                              embedding_graph.edge_norm)
 
-    rmse = model.score_rmse(entity_embedding, test_samples, test_labels)
+    mse = model.score_mse(entity_embedding, test_samples, test_labels)
+    # print('\n MSE of test data: {}'.format(mse))
+
+    rmse = np.sqrt(mse)
+    # print('\n RMSE of test data: {}'.format(rmse))
+
     mape = model.score_mape(entity_embedding, test_samples, test_labels)
+    # print('\n MAPE of test data: {}'.format(mape))
+
     cpc = model.score_cpc(entity_embedding, test_samples, test_labels)
+    # print('\n CPC of test data: {}'.format(cpc))
+
+    mae = model.score_mae(entity_embedding, test_samples, test_labels)
+    # print('\n MAE of test data: {}'.format(mae))
+
+    evs = model.score_evs(entity_embedding, test_samples, test_labels)
+    # print('\n EVS of test data: {}'.format(evs))
+
     r2 = model.score_r2(entity_embedding, test_samples, test_labels)
+    # print('\n R2 of test data: {}'.format(r2))
 
-    model.export_results(entity_embedding, test_samples, test_labels).to_csv('result/'+file_mark+'_eval.csv',
+    scc = model.score_scc(entity_embedding, test_samples, test_labels)
+    # print('\n SCC of test data: {}'.format(scc))
+
+    # file_name = args.dataset.split('/')[-1] + '_' + 'metric' + '_' + setting['negative_num'] + '_' + \
+    #             setting['n_bases'] + '_' + setting['embedding_size']
+
+    # print flow dataframe
+    model.export_results(entity_embedding, test_samples, test_labels).to_csv('output/'+file_mark+'_pred.csv',
                                                                              header=True, encoding='gbk')
+    # print attribute matrix
+    # with open('./result/'+file_mark+'_attr.txt', 'wb') as f:
+    #     for line in np.matrix(checkpoint['state_dict']['conv.att'].detach().numpy()):
+    #         np.savetxt(f, line, fmt='%.4f')
 
-    return [rmse, mape, cpc, r2]
+    return [mse, rmse, mape, cpc, mae, evs, r2, scc]
 
 
 if __name__ == '__main__':
-    with open('metrics.csv', 'w') as f:
-        f.write('model,rmse,mape,cpc,r2\r\n')
-        for file in os.listdir('saved_model/'):
+    with open('output/metrics_all.csv', 'w') as f:
+        f.write('model,mse,rmse,mape,cpc,mae,evs,r2,scc\r\n')
+        for file in os.listdir('output/'):
             if file[-3:] == 'pth':
-                print('\n------------------------------------')
-                print('Model:', file)
+                print('\n' + file)
                 file_mark = file[0:-4]
-                # Attention: Replace the graph file that is used for training
-                # shutil.copy2('data/graph_knn/graph_30.txt', 'data/mobility/graph.txt')
-
                 parser = argparse.ArgumentParser(description='RGCN')
-                parser.add_argument("--dataset", type=str, default="./data/mobility")
+                parser.add_argument("--dataset", type=str, default="./data/test")
                 parser.add_argument("--setting", type=str, default="./setting.txt")
                 parser.add_argument("--model", type=str, default=file)
 
                 args = parser.parse_args()
-                # print('Data path:')
-                # print('\t' + args.dataset)
-                # print('Setting file:')
-                # print('\t' + args.setting)
+                print('Data path:')
+                print('\t' + args.dataset)
+                print('Setting file:')
+                print('\t' + args.setting)
 
-                r = evaluate(args, file_mark)
-                print('Metrics:')
-                print('\t[rmse, mape, cpc, r2]=', r)
-                f.write(','.join([file_mark] + list(map(str, r)))+'\r\n')
+                r = main(args, file_mark)
+                f.write(','.join([file_mark] + list(map(str, r)))+'\r\n')   # Change \r\n if necessary
